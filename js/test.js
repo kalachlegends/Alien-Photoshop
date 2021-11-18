dda34304b56d8c8ec8ba1db46f814c8e45212694
kalachlegends <90833872+kalachlegends@users.noreply.github.com>
2021-11-18 20:45:34 +0600

    Add files via upload

---
+++ js/test.js
@@ -0,0 +1,4 @@
+calc.onclick = function(){
+    var myform = this.form;
+    myform.result.value = +myform.myname.value + +myform.street.value + +myform.myname1.value + +myform.myname2.value + +myform.myname3.value + +myform.myname4.value ;
+}
